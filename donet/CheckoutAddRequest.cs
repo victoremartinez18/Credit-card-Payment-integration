caa293dd040b371b877493aa36cfab69978e93a5
victoremartinez18 <victoremartinez18@gmail.com>
2024-05-20 18:27:36 -0400

    Updated Readme and added .net files.

---
+++ donet/CheckoutAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class CheckoutAddRequest
+    {
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Total { get; set; }
+
+    }
+}
